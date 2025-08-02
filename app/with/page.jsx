7663ba1b5dca6793897c299fcb31987c3fab65e1
kyoonbb <kyoonbb0111@naver.com>
2025-08-02 17:31:33 +0900

    chore: 윤기 깃 테스트

--- app/with/page.jsx
+++ app/with/page.jsx
@@ -2,6 +2,7 @@ export default function With() {
   return (
     <>
       <h1 className="text-3xl font-bold">동행모집</h1>
+      <h2 className="text-3xl font-bold">윤기 브런치 생성 및 푸시 테스트 두번째</h2>
     </>
   );
 }
