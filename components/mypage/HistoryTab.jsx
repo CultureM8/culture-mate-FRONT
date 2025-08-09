aaeb59d7fe89df464c1714729beef594b1fe9e86
JUNGHEEYOUNG9090 <55870328+JUNGHEEYOUNG9090@users.noreply.github.com>
2025-08-09 10:26:16 +0900

    feat:히스토리 개발 - 관심기록과 유사

---
+++ components/mypage/HistoryTab.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import HistoryEvent from "./HistoryEvent";
+import HistoryWith from "./HistoryWith";
+
+export default function HistoryTab() {
+  const [activeTab, setActiveTab] = useState("HistoryEventTab");
+
+  return (
+    <div className="mt-1">
+      <div className="relative flex gap-4 border-b border-gray-300">
+        <button
+          onClick={() => setActiveTab("HistoryEventTab")}
+          className={`w-1/4 px-4 py-2 ${
+            activeTab === "InterEventTab"
+              ? "border-b-2 border-black text-black"
+              : "text-gray-500"
+          }`}
+        >
+          이벤트 리뷰
+        </button>
+        <button
+          onClick={() => setActiveTab("HistoryWithTab")}
+          className={`w-1/4 px-4 py-2 ${
+            activeTab === "InterWithTab"
+              ? "border-b-2 border-black text-black"
+              : "text-gray-500"
+          }`}
+        >
+          동행 기록
+        </button>
+      </div>
+      <div className="mt-8">
+        {activeTab === "HistoryEventTab" && <HistoryEvent />}
+        {activeTab === "HistoryWithTab" && <HistoryWith />}
+      </div>
+    </div>
+  );
+}
