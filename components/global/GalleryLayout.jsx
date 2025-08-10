58fa59747148e3a6fe6000a310641aa11d42b03e
kihoonkwon <jhgfjhgfjhgf96@gmail.com>
2025-08-10 14:18:15 +0900

    chore: 필요없는 import 삭제

--- components/global/GalleryLayout.jsx
+++ components/global/GalleryLayout.jsx
@@ -1,5 +1,3 @@
-import EventGallery from "../events/EventGallery";
-
 export default function GalleryLayout({ Component, items, commonProps={} }) {
   return (
     <div className="grid grid-cols-[repeat(auto-fit,300px)] gap-0 justify-center">
