d83afb52a8b3e979741f76f719050f4d033bf65f
권기훈 <jhgfjhgfjhgf96@gmail.com>
2025-08-12 17:20:02 +0900

    feat: GalleryLayout에 공통 속성 전달 기능 추가 (#16)
    `GalleryLayout`에 `commonProps`를 추가하여, 갤러리의 모든 아이템에 공통 속성을 한 번에 전달할 수 있도록
    개선했습니다.
    예를 들어, `commonProps`를 통해 모든 아이템의 관심 버튼 표시 여부를 한번에 제어할 수 있습니다.
    commonProps={{ prop: value }} 형태로 전달.

--- components/global/Gallery.jsx
+++ components/global/Gallery.jsx
@@ -4,10 +4,12 @@ import { ICONS, IMAGES } from "@/constants/path";
 import Image from "next/image";
 import { useState } from "react";
 
-export default function Gallery({ src, alt = "이미지", title = "제목 없음", onClick, children }) {
+export default function Gallery({ src, alt = "이미지", title = "제목 없음", enableInterest=true, onClick, children }) {
 
+  // 추후에 매개변수로 넘겨받아서 처리
   const [interest, setInterest] = useState(false);
 
+  // 이부분은 추후에 page에서 처리하는 방식으로 변경해야 할 듯
   const interestHandler = () => {
     setInterest(prev => !prev);
     if(typeof onClick == "function") onClick();
@@ -15,16 +17,18 @@ export default function Gallery({ src, alt = "이미지", title = "제목 없음
   
   return (
     <div className="bg-white w-[300px] relative" title={title}>
-      <button className={`absolute top-0 right-0 mt-4 mr-4 ${interest ? "" : "opacity-30"} hover:cursor-pointer`}
-        onClick={interestHandler}
-      >
-        <Image 
-          src={interest ? ICONS.HEART : ICONS.HEART_EMPTY}
-          alt="관심"
-          width={28}
-          height={28}
-        />
-      </button>
+      {enableInterest &&
+        <button className={`absolute top-0 right-0 mt-4 mr-4 ${interest ? "" : "opacity-30"} hover:cursor-pointer`}
+          onClick={interestHandler}
+        >
+          <Image 
+            src={interest ? ICONS.HEART : ICONS.HEART_EMPTY}
+            alt="관심"
+            width={28}
+            height={28}
+          />
+        </button>
+      }
       <div className="mx-[10px] py-[10px] overflow-hidden whitespace-nowrap text-ellipsis text-gray-400">
         <Image
           src={src ? src : IMAGES.GALLERY_DEFAULT_IMG}
