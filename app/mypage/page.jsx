e9ec6a2aedba6e68787ba658ab0bf95f1a460779
younji.mok <younji.mok@gmail.com>
2025-08-14 15:41:14 +0900

    feat: 나의모집글, 동행 기록 탭 뷰 추가 (#36)
    - 나의모집글, 동행기록 카드, 리스트뷰 추가

--- app/mypage/page.jsx
+++ app/mypage/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import ProfileBG from "@/components/mypage/ProfileBG";
 import ProfileDetail from "@/components/mypage/ProfileDetail";
@@ -7,22 +7,17 @@ import RecruitTabView from "@/components/mypage/RecruitTabView";
 export default function MyPage() {
   return (
     <div className="w-full min-h-screen">
-      {/* 1. 프로필 배경 및 기본 정보 섹션 */}
+      {/* 프로필 배경 및 기본 정보 */}
       <ProfileBG />
       
-      {/* 2. 프로필 상세 정보 섹션 */}
+      {/* 프로필 상세 정보 */}
       <div className="w-full max-w-[1200px] mx-auto px-4 py-6">
         <ProfileDetail />
       </div>
       
-      {/* 3. 모집글/리뷰/기록 탭 섹션 */}
-      <div className="w-full border-t border-gray-200 py-6">
+      {/* 탭 메뉴 및 컨텐츠 (나의 모집글, 이벤트 리뷰, 동행 기록) */}
+      <div className="w-full border-t border-gray-200">
         <RecruitTabView />
-      </div>
-      
-      {/* 4. 카드형/리스트형 이미지 섹션 */}
-      <div className="w-full max-w-[1200px] mx-auto px-4 py-6">
-        {/* TODO: 카드형 이미지/리스트형 이미지 컴포넌트가 들어갈 공간 */}
       </div>
     </div>
   );
