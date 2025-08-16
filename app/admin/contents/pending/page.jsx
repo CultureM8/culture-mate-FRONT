103f3a036c68442a9c73d829251b2f2d2816d7c5
raytear <younji.mok@gmail.com>
2025-08-16 16:30:31 +0900

    feat: 이벤트 유형관리 / 승인 대기 홍보 삭제

--- app/admin/contents/pending/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function ContentsPending() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold">승인 대기 홍보</h1>
-    </>
-  );
-}
--- app/admin/events/type/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function EventsType() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold">이벤트 유형 관리</h1>
-    </>
-  );
-}
