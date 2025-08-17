390c2c8dd443cb837a3457862c7af6e40a63d53b
JUNGHEEYOUNG9090 <55870328+JUNGHEEYOUNG9090@users.noreply.github.com>
2025-08-17 13:00:09 +0900

    refactor: 게시물 관리에서 댓글 관리 제거 ⇒ 히스토리 이벤트리뷰 리스트형

---
+++ backup/HistoryEvent.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import EventGallery from "@/components/events/EventGallery";
+import GalleryLayout from "@/components/global/GalleryLayout";
+import EditSetting from "@/components/global/EditSetting";
+import { useState } from "react";
+
+export default function HistoryEvent({ eventData }) {
+  const [selectedType, setSelectedType] = useState("전체");
+
+  return (
+    <>
+      <EditSetting />
+      <GalleryLayout Component={EventGallery} items={eventData} />
+    </>
+  );
+}
